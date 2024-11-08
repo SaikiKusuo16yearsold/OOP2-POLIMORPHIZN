a108a446a93c708d076b1e7f7db0994859331501
имя фамилия <Dima>
2024-11-08 22:41:25 +0300

    small commit

--- src/Transports/Car.java
+++ src/Transports/Car.java
@@ -8,8 +8,8 @@ public class Car extends Transports implements Serviceable, EngineCheckable {
 
     public Car(String modelName, int wheelsCount) {
         super(modelName, wheelsCount);
-    }
 
+    }
 
 
 }
